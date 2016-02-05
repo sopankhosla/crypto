a88d6adaceb6bc53aea573c295808d337a32a324
sopankhosla <khoslasopan@gmail.com>
2016-02-05 12:01:15 +0530

    challenges solved

---
+++ hex-base64.py
@@ -0,0 +1,6 @@
+import binascii
+#define starting string in hex
+h = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+hex = h.decode("hex")
+base = binascii.b2a_base64(hex)
+print(base)
